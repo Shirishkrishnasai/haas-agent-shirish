0354c5b5d382358d183fb611fe82f99fa01e401d
teju_regulagadda <tejasri.regulagadda@highgear.io>
2019-01-28 14:19:41 +0530

    agent monitor file created, supervisor, configfile and models files are modified

--- application/configfile.py
+++ application/configfile.py
@@ -11,8 +11,6 @@ kafka_api_version = (0, 10, 1)
 mongo_conn_string = "mongodb://104.211.222.41:27017"
 
 download_url="http://104.211.222.41:5000/filedownload/"
-kafka_bootstrap_server = ['52.172.28.18:9092']
-kafka_api_version = (0, 10, 1)
 hive_connection = "localhost"
 hive_output_path = "/opt/hiveoutput/"
 file_upload_url = "http://192.168.100.166:5000/fileupload"
