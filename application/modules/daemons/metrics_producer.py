148bc4473da7a2a75631cbca37e83592833906c2
dev1 <jastisriram94@gmail.com>
2019-03-12 14:45:09 +0530

    metrics updated

--- application/modules/daemons/metrics_producer.py
+++ application/modules/daemons/metrics_producer.py
@@ -19,45 +19,47 @@ def kafkaMetricsProducer():
     data_req = json.loads(content, 'utf-8')
     customer_id = str(data_req['customer_id'])
     cluster_id = str(data_req['cluster_id'])
-    # agent_id = str(data_req['agent_id'])
     role=str(data_req['role'])
     info.close()
-    try:
-        if role=="namenode":
-            my_logger.debug("Getting metrics and publishing....")
-            producer = KafkaProducer(bootstrap_servers=[kafka_server_url])
-            kafka_topic = "metrics_" + customer_id + "_" + cluster_id
-            my_logger.debug(kafka_topic)
-            kafkatopic = kafka_topic.decode('utf-8')
-            print "getting ram_metrics"
-            ram_metrics = ramMetrics()
-            print "getting cpu metrics"
-            cpu_metrics = cpuMetrics()
-            print "getting storage metrics"
-            storage_metrics = storage()
-            metrics_list = []
-            metrics_list.extend((ram_metrics,storage_metrics,cpu_metrics))
-            my_logger.debug(metrics_list)
-            metrics_data = {}
-            metrics_data['event_type'] = "metrics"
-            date_time = datetime.datetime.now()
-            print metrics_list
-            time_value = str(int(round(time.mktime(date_time.timetuple()))) * 1000)
-            metrics_data['time'] = time_value
-            metrics_data['customer_id'] = customer_id
-            metrics_data['cluster_id'] = cluster_id
-            metrics_data['payload'] = metrics_list
-            producer.send(kafkatopic, str(metrics_data))
-            producer.flush()
-            print "metricss produced"
-        else :
-             pass
-    except Exception as e:
-         #my_logger.error("Some error caught", e.message)
-         print e
+    while True:
+        try:
+            if role=="namenode":
+                my_logger.debug("Getting metrics and publishing....")
+                producer = KafkaProducer(bootstrap_servers=[kafka_server_url])
+                kafka_topic = "metrics_" + customer_id + "_" + cluster_id
+                my_logger.debug(kafka_topic)
+                kafkatopic = kafka_topic.decode('utf-8')
+                print "getting ram_metrics"
+                ram_metrics = ramMetrics()
+                print "getting cpu metrics"
+                cpu_metrics = cpuMetrics()
+                print "getting storage metrics"
+                storage_metrics = storage()
+                metrics_list = []
+                metrics_list.extend((ram_metrics,storage_metrics,cpu_metrics))
+                my_logger.debug(metrics_list)
+                metrics_data = {}
+                metrics_data['event_type'] = "metrics"
+                date_time = datetime.datetime.now()
+                print metrics_list
+                time_value = str(int(round(time.mktime(date_time.timetuple()))) * 1000)
+                metrics_data['time'] = time_value
+                metrics_data['customer_id'] = customer_id
+                metrics_data['cluster_id'] = cluster_id
+                metrics_data['payload'] = metrics_list
+                producer.send(kafkatopic, str(metrics_data))
+                producer.flush()
+                print "metricss produced"
+            else :
+                pass
+        except Exception as e:
+            #my_logger.error("Some error caught", e.message)
+            print e
+        time.sleep(60)
+
 
 def kafkaMetricsProducerScheduler():
-    scheduler = BackgroundScheduler()
-    scheduler.add_job(kafkaMetricsProducer,'cron',minute='*/1' )
-    scheduler.start()
+    # scheduler = BackgroundScheduler()
+    # scheduler.add_job(kafkaMetricsProducer,'cron',minute='*/1' )
+    # scheduler.start()
     kafkaMetricsProducer()
--- application/modules/workers/storage_metric_worker.py
+++ application/modules/workers/storage_metric_worker.py
@@ -1,27 +1,29 @@
 import subprocess
 def storage():
-        p = subprocess.Popen(["hdfs", "dfsadmin", "-report"], stdout=subprocess.PIPE)
-        stdout = p.communicate()
-        stdout=str(stdout).replace('-------------------------------------------------','\n')
-        stdout = str(stdout).split("\\n\\n\\n")[0:]
-        lis=[]
-        for data in stdout:
-            if data == None:
-                 pass
-            else:
-                value = data.split("\\n")
-                dicto = {}
-                for valu in value:
-                    if ':' in valu :
-                       va=(valu.split(':'))
-                       dicto[va[0]] = va[1]
-                if dicto != {}:
-                        dic={}
-                        dfs_ram=dicto["DFS Remaining"]
-                        dfs_remaining=dfs_ram.split("(")
-                        dic["host_name"]= dicto["Hostname"].strip()
-                        dic["metric_value"]=dfs_remaining[0]
-                        dic["metric_name"]="storage"
-                        dic["measured_in"]="bytes"
-                        lis.append(dic)
-	    return lis
+    args="/opt/agent/application/modules/workers/hdfs1.sh"
+    out_put = subprocess.Popen(args, stdout=subprocess.PIPE, stderr=subprocess.PIPE)
+    out, err = out_put.communicate()
+    storage_data=str(out).replace("-------------------------------------------------","\n")
+    memory_line=storage_data.split("\n\n\n")[1:]
+    available_storage=[]
+    for mem_data in memory_line:
+       if mem_data == None:
+          pass
+       else:
+          individual_line = str(mem_data).split("\n")
+          storage_data = {}
+          for element in individual_line:
+              if ':' in element :
+                  hdfs_remain=(element.split(':'))
+                  storage_data[hdfs_remain[0]] = hdfs_remain[1]
+          if storage_data != {}:
+              store={}
+              dfs_ram=storage_data["DFS Remaining"]
+              dfs_remaining=dfs_ram.split("(")
+              store["host_name"]= storage_data["Hostname"].strip()
+              store["metric_value"]=dfs_remaining[0].strip()
+              store["metric_name"]="storage"
+              store["measured_in"]="bytes"
+              available_storage.append(store)
+    # print available_storage
+    return available_storage
