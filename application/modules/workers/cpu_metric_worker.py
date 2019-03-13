b29888da58e782255a54e636ef7cdf12ca129448
shirishkrishna <shirishkrishnasai@gmail.com>
2019-03-13 11:36:11 +0530

    modified all workers

--- application/modules/workers/cpu_metric_worker.py
+++ application/modules/workers/cpu_metric_worker.py
@@ -1,7 +1,6 @@
 import json
 import requests
 def cpuMetrics():
-
 	url = "http://localhost:8088/ws/v1/cluster/nodes"
 	response = requests.get(url)
 	result = json.loads(response.text)
@@ -13,7 +12,7 @@ def cpuMetrics():
 		metric_value = val["availableVirtualCores"]
 		dict = {}
 		dict["metric_name"]="cpu"
-		dict["host_name"]=str(host_name)
-		dict["metric_value"]=metric_value
-		lis.append(dict)
+       	dict["host_name"]=str(host_name)
+        dict["metric_value"]=metric_value
+        lis.append(dict)
 	return lis
